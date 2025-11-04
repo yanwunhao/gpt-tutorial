97593702e5f358426518e08f3fd35cc5e905bdc4
十裏山路不換肩 <wh.yan@hotmail.com>
2025-11-04 10:19:22 +0900

    Update train_gpt2.py

--- train_gpt2.py
+++ train_gpt2.py
@@ -1,9 +1,15 @@
 import os
+import math
+
 import torch
+import torch.nn as nn
+from torch.nn import functional as F
+from torch.utils.data import Dataset, DataLoader
+
 from datasets import load_dataset
 from tqdm import tqdm
 import tiktoken
-from torch.utils.data import Dataset, DataLoader
+
 
 # setting the parameters
 
@@ -11,7 +17,7 @@ num_proc = 14
 
 local_data_path = "./sample_data"
 
-blocksize = 128
+blocksize = 1024
 batchsize = 32
 
 device_type = (
@@ -31,11 +37,11 @@ def process(example):
     return {"ids": ids, "len": len(ids)}
 
 
-example = {"text": "Gonsalves AI Lab"}
+example = {"text": "Make America Great Again"}
 print(process(example))
 
 
-class StreamingParquetDataset(torch.utils.IterableDataset):
+class StreamingParquetDataset(torch.utils.data.IterableDataset):
     def __init__(self, data_files, split, block_size, num_proc=14):
         self.data_file = data_files
         self.split = split
@@ -54,3 +60,55 @@ class StreamingParquetDataset(torch.utils.IterableDataset):
                     ids[i + 1 : i + 1 + self.block_size], dtype=torch.int64
                 )
                 yield x, y
+
+
+arrow_files = [
+    os.path.join(local_data_path, f)
+    for f in os.listdir(local_data_path)
+    if f.endswith(".arrow")
+]
+
+
+train_dataset = StreamingParquetDataset(arrow_files, "train", blocksize, num_proc)
+val_dataset = StreamingParquetDataset(arrow_files[-1], "val", blocksize, num_proc)
+
+train_dataloader = DataLoader(
+    train_dataset, batch_size=batchsize, shuffle=False, num_workers=0, pin_memory=True
+)
+val_dataloader = DataLoader(
+    val_dataset, batch_size=batchsize, shuffle=False, num_workers=0, pin_memory=True
+)
+
+
+def get_batch(dataloader, device):
+    for x, y in dataloader:
+        if device_type == "cuda":
+            x, y = x.to(device, non_block=True), y.to(device, non_block=True)
+        else:
+            x, y = x.to(device), y.to(device)
+
+        yield x, y
+
+
+class config:
+    block_size = 1024
+    vocab_size = 50304
+    n_layer = 12
+    n_head = 12
+    n_embd = 768
+    dropout = 0.0
+    bias = False
+
+
+wte = nn.Embedding(config.vocab_size, config.n_embd).to(device)
+wpe = nn.Embedding(config.block_size, config.n_embd).to(device)
+drop = nn.Dropout(config.dropout).to(device)
+ln_f = nn.LayerNorm(config.n_embd, bias=config.bias).to(device)
+
+for x, y in get_batch(train_dataloader, device):
+    print(x.shape, y.shape)
+    x_embd = wte(x)
+    print(x_embd.shape)
+    x_embd_ln = ln_f(x_embd)
+    print(x_embd_ln.shape)
+    break
